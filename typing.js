4227a9a47d4fae5f03d92b7b8873f0dbbedd99c9
iksadnorth <iksadnorth@gmail.com>
2025-07-06 14:25:14 +0900

    타이핑 효과 부여 v2

--- typing.js
+++ typing.js
@@ -1,15 +1,27 @@
+const typeInterval = 75;
+
 function type(container) {
     const text = container.textContent;
     container.textContent = '';
     text.split('').forEach((char, i) => {
         setTimeout(() => {
             container.textContent += char;
-    
-            if (i === text.length - 1) {
-                container.classList.add('blink');
-            }
-        }, 75 * i);
+        }, typeInterval * i);
     });
+    
+    setTimeout(() => {
+        container.classList.add('blink');
+        syncBlink();
+    }, 0);
+}
+
+function syncBlink() {
+    const arrElBlink = document.querySelectorAll('.blink');
+    arrElBlink.forEach(el => el.classList.remove('blink'));
+    
+    setTimeout(() => {
+        arrElBlink.forEach(el => el.classList.add('blink'));
+    }, 0);
 }
 
 const containers = document.querySelectorAll('.typewriter');
